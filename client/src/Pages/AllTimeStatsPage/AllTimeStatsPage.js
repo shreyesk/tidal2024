453f8e080bd41cb5a8e15ae71ac4882ecc50e81d
m-argarita-m <anirudh.margam@gmail.com>
2024-02-24 23:30:23 -0600

    created statistic bar component and added to both pages

--- client/src/Pages/AllTimeStatsPage/AllTimeStatsPage.js
+++ client/src/Pages/AllTimeStatsPage/AllTimeStatsPage.js
@@ -1,27 +1,60 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import { fetchGet, fetchPost } from '../../util/fetchHelp.js';
-
-import './AllTimeStatsPage.css';
+import { fetchGet } from "../../util/fetchHelp.js";
+import StatisticBar from "../../Components/Statistic/StatisticBar.js";
+import "./AllTimeStatsPage.css";
 
 function AllTimeStatsPage() {
-  const [getData, setGetData] = useState()
+  const [getData, setGetData] = useState();
 
-  useEffect(() =>{
-    fetchGet("/get-all-time-drive-stats").then(data => {
+  useEffect(() => {
+    fetchGet("/get-all-time-drive-stats").then((data) => {
       setGetData(data.message);
     });
-  })
+  });
 
   return (
     <div className="App">
-      {(typeof getData === "undefined") ? (
+      {typeof getData === "undefined" ? (
         <p>Loading...</p>
-      ): (
-        <>
-          <p>Data received from our get request:</p>
-          <p>{getData}</p>
-        </>
+      ) : (
+        <div className="statistics-container">
+            <StatisticBar
+                statisticLabel="# of times you texted"
+                userOccurrences="50"
+                medianValue="40"
+            />
+            <StatisticBar
+                statisticLabel="# of times you talked on the phone"
+                userOccurrences="50"
+                medianValue="60"
+            />
+            <StatisticBar
+                statisticLabel="# of times you messed with infotainment"
+                userOccurrences="50"
+                medianValue="40"
+            />
+            <StatisticBar
+                statisticLabel="# of times you drank"
+                userOccurrences="50"
+                medianValue="40"
+            />
+            <StatisticBar
+                statisticLabel="# of times you reached into the backseat"
+                userOccurrences="50"
+                medianValue="40"
+            />
+            <StatisticBar
+                statisticLabel="# of times you did hair or makeup"
+                userOccurrences="50"
+                medianValue="40"
+            />
+            <StatisticBar
+                statisticLabel="# of times you were distracted by your passengers"
+                userOccurrences="50"
+                medianValue="40"
+            />
+        </div>
       )}
     </div>
   );
--- client/src/Pages/MostRecentDrivePage/MostRecentDrivePage.js
+++ client/src/Pages/MostRecentDrivePage/MostRecentDrivePage.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
 import { fetchGet, fetchPost } from '../../util/fetchHelp.js';
-
+import StatisticBar from '../../Components/Statistic/StatisticBar.js';
 import './MostRecentDrivePage.css';
 
 function MostRecentDrivePage() {
@@ -18,10 +18,43 @@ function MostRecentDrivePage() {
       {(typeof getData === "undefined") ? (
         <p>Loading...</p>
       ): (
-        <>
-          <p>Data received from our get request:</p>
-          <p>{getData}</p>
-        </>
+        <div className="statistics-container">
+            <StatisticBar
+                statisticLabel="# of times you texted"
+                userOccurrences="50"
+                medianValue="40"
+            />
+            <StatisticBar
+                statisticLabel="# of times you talked on the phone"
+                userOccurrences="50"
+                medianValue="60"
+            />
+            <StatisticBar
+                statisticLabel="# of times you messed with infotainment"
+                userOccurrences="50"
+                medianValue="40"
+            />
+            <StatisticBar
+                statisticLabel="# of times you drank"
+                userOccurrences="50"
+                medianValue="40"
+            />
+            <StatisticBar
+                statisticLabel="# of times you reached into the backseat"
+                userOccurrences="50"
+                medianValue="40"
+            />
+            <StatisticBar
+                statisticLabel="# of times you did hair or makeup"
+                userOccurrences="50"
+                medianValue="40"
+            />
+            <StatisticBar
+                statisticLabel="# of times you were distracted by your passengers"
+                userOccurrences="50"
+                medianValue="40"
+            />
+        </div>
       )}
     </div>
   );
