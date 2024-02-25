40ff3519b64521d81fb06f49dfaa3aa1033a7a9d
m-argarita-m <anirudh.margam@gmail.com>
2024-02-25 08:54:16 -0600

    fixed csv and first backend function for most recent drive

--- server/server.py
+++ server/server.py
@@ -1,10 +1,20 @@
 from flask import Flask, jsonify, request
+import pandas as pd
 
 app = Flask(__name__)
 
 @app.route('/get-most-recent-drive-stats')
 def getMostRecentDriveStats():
-    return jsonify({"message": "this works!"})
+    csv_file_path = './data.csv'  # Adjust the path to where your data.csv is located
+    df = pd.read_csv(csv_file_path)
+    last_row = df.iloc[-1]
+
+    csv_column_names = ['normal driving', 'texting', 'talking on phone', 'operating the radio', 'drinking', 'reaching behind', 'hair and makeup', 'talking to passenger']
+    mostRecentDriveStats = []
+    for columnName in csv_column_names:
+        mostRecentDriveStats.append(last_row[columnName])
+        print(f'mostRecentDriveStats is now: {mostRecentDriveStats}')
+    return jsonify({"message": mostRecentDriveStats})
 
 @app.route('/get-all-time-drive-stats')
 def getAllTimeDriveStats():
